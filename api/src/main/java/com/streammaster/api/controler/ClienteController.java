446e34f323b3456501a4294f4187aab829e778e4
vit0rrr <vitorhmeira@gmail.com>
2026-02-26 16:36:26 -0300

    feat: recriando a estrutura base da API Spring Boot

--- api/src/main/java/com/streammaster/api/controler/ClienteController.java
+++ api/src/main/java/com/streammaster/api/controler/ClienteController.java
@@ -15,8 +15,37 @@ public class ClienteController {
         this.service = service;
     }
 
+
+    //CREATE (POST) - ROTA: POST / CLIENTES
+    @PostMapping
+    public ResponseEntity<Cliente> criar(@RequestBody Cliente dto) {
+        Cliente novoCliente = service.salvar(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novoCliente);
+    }
+
+
+    //READ (GET) - ROTA: GET / CLIENTES
     @GetMapping
-    public List<Cliente> listar() {
-        return service.listarTodos();
+    public ResponseEntity<List<Cliente>> listarTodos() {
+        return ResponseEntity.ok(service.listarTodos());
+    }
+
+    //READ (GET) por ID - ROTA: GET / CLIENTES/{ID}
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> buscarPorId(@PathVariable Long id) {
+        return ResponseEntity.ok(service.buscarPorId(id));
+    }
+
+    //UPDATE (PUT) - ROTA: PUT / CLIENTES/{ID}
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente> atualizar(@PathVariable Long id, @RequestBody Cliente dto) {
+        Cliente clienteAtualizado = service.atualizar(id, dto);
+        return ResponseEntity.ok(clienteAtualizado);
+    }
+    //DELETE (DELETE) - ROTA: DELETE / CLIENTES/{ID}
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id) {
+        service.deletar(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- api/src/main/java/com/streammaster/api/service/ClienteService.java
+++ api/src/main/java/com/streammaster/api/service/ClienteService.java
@@ -3,6 +3,7 @@ package com.streammaster.api.service;
 import com.streammaster.api.repository.ClienteRepository;
 import com.streammaster.api.model.Cliente;
 import org.springframework.stereotype.Service;
+import com.streammaster.api.dto.ClienteDTO;
 import java.util.List;
 
 @Service
@@ -14,7 +15,38 @@ public class ClienteService {
         this.repository = repository;
     }
 
+    // CREAT 
+    public client salvar(Cliente dto) {
+        Cliente cliente = new Cliente();
+        cliente.setNome(dto.nome());
+        cliente.setEmail(dto.email());
+        return repository.save(cliente);
+    }
+
+    //READ
     public List<Cliente> listarTodos() {
         return repository.findAll();
+    }
+
+
+    //READ   public Cliente buscarPorId(Long id) {
+    public Cliente buscarPorId(Long id) {
+        return repository.findById(id).orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
+    }
+
+
+    //UPDATE
+    public Cliente atualizar(Long id, Cliente dto) {
+        Cliente cliente = repository.findById(id).orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
+        cliente.setNome(dto.nome());
+        cliente.setEmail(dto.email());
+        return repository.save(cliente);
+    }
+
+
+    //DELETE 
+    public void deletar(Long id) {
+        Cliente cliente = repository.findById(id).orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
+        repository.delete(cliente);
     }
 }
